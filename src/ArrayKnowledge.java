dc1020cb9d7c214979b596dfed4b39a31546d60e
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-10-17 16:49:53 -0600

    Added Comments

--- src/ArrayKnowledge.java
+++ src/ArrayKnowledge.java
@@ -3,37 +3,60 @@ import java.util.Scanner;
 
 public class ArrayKnowledge {
     public static void main(String[] args) {
+        //This executes the stringToArray program.
         System.out.println(Arrays.toString(stringToArray()));
+        //This executes the generateArrayWithBounds program.
         System.out.println(Arrays.toString(generateArrayWithBounds()));
     }
-
+    //This creates a new method called stringToArray.
     public static char[] stringToArray() {
+        //This adds a scanner to the program.
         Scanner sc = new Scanner(System.in);
+        //This prompts the user to input their string.
         System.out.println("Please enter your string:");
+        //This defines their input as their response to the prompt.
         String originalInput = sc.nextLine();
+        //This takes the length of their input.
         int inputLength = originalInput.length();
+        //This creates a character array with the length of their input.
         char[] stringArray = new char[inputLength];
+        //This starts a for loop, executing the interior when i is less than or equal to the input length.
         for (int i = 0; i <= inputLength - 1; i++) {
+            //This takes the character at the ith place in the string
+            // and inserts it into the array at the ith place in the array.
             stringArray[i] = originalInput.charAt(i);
         }
+        //This returns the stringArray to the main method to be printed out.
         return stringArray;
     }
-
+    //This creates the generateArrayWithBounds method.
     public static int[] generateArrayWithBounds() {
+        //This adds a scanner to the program.
         Scanner sc = new Scanner(System.in);
+        //This prompts the user to enter the starting integer.
         System.out.println("Enter the starting integer:");
+        //This defines their starting integer.
         int start = sc.nextInt();
+        //This prompts the user to enter the ending integer.
         System.out.println("Enter the ending integer:");
+        //This defines their ending integer.
         int end = sc.nextInt();
+        //This promps the user to enter their step integer.
         System.out.println("Enter the integer step:");
+        //This defines their step integer.
         int step = sc.nextInt();
+        //This calculates the number of integers in the array.
         int numberIntegers = ((end - start) / step) + 1;
+        //This creates the boundedArray as an integer array with the number of integers calculated above.
         int[] boundedArray = new int[numberIntegers];
-
+        //This starts a for loop executing when i is less than or equal to the number of integers.
         for (int i = 0; i <= numberIntegers - 1; i++) {
+            //This inserts the starting integer in the array at the ith place.
             boundedArray[i] = start;
+            //This redefines the starting integer as the previous starting integer plus the stepping integer.
             start = start + step;
         }
+        //This passes the boundedArray to the main method to be printed out.
         return boundedArray;
     }
 }
--- src/LogicKnowledge.java
+++ src/LogicKnowledge.java
@@ -2,58 +2,97 @@ import java.util.Scanner;
 
 public class LogicKnowledge {
     public static void main(String[] args) {
+        //This prints out the lottery score from the greenTicket method.
         System.out.println("Your score is " + greenTicket());
+        //This executes the makeChocolate method.
         System.out.println(makeChocolate());
     }
-
+    //This creates a greenTicket method to calculate lottery scores.
     public static int greenTicket() {
+        //This adds a scanner to the program.
         Scanner sc = new Scanner(System.in);
+        //This creates a new integer array to hold the 3 lottery numbers.
         int[] ticketStorage = new int[3];
+        //This prompts the user to enter the first lottery number.
         System.out.println("Enter the first lottery number:");
+        //This defines their input as the lottery number in the 0th place in the array.
         ticketStorage[0] = sc.nextInt();
+        //This prompts the user to enter the second lottery number.
         System.out.println("Enter the second lottery number:");
+        //This defines their input as the lottery number in the 1st place of the array.
         ticketStorage[1] = sc.nextInt();
+        //This prompts the user to enter the final lottery number.
         System.out.println("Enter the third lottery number:");
+        //This defines their input as the lottery number in the 2nd place of the array.
         ticketStorage[2] = sc.nextInt();
+        //This executes the following if all the numbers are the same.
         if (ticketStorage[0] == ticketStorage[1] && ticketStorage[1] == ticketStorage[2]) {
+            //This returns the score of 20.
             return 20;
-        } else if (ticketStorage[0] == ticketStorage[1] || ticketStorage[0] == ticketStorage[2] || ticketStorage[1] == ticketStorage[2]) {
+            //This executes the following if two of the numbers are the same.
+        } else if (ticketStorage[0] == ticketStorage[1] ||
+                ticketStorage[0] == ticketStorage[2] || ticketStorage[1] == ticketStorage[2]) {
+            //This returns the score of 10.
             return 10;
+            //This executes the following if none of the numbers are the same.
         } else {
+            //This returns the score of 0.
             return 0;
         }
     }
-
+    //This creates a new makeChocolate method.
     public static int makeChocolate() {
+        //This adds a scanner to the program.
         Scanner sc = new Scanner(System.in);
+        //This prompts the user to enter the number of 1 pound chocolate bars.
         System.out.println("Enter the number of 1 pound chocolate bars:");
+        //This defines the number of 1 pound bars as their input.
         int onePound = sc.nextInt();
+        //This prompts the user to enter the number of 5 pound chocolate bars.
         System.out.println("Enter the number of 5 pound chocolate bars:");
+        //This defines the number of 5 pound bars as their input.
         int fivePound = sc.nextInt();
+        //This prompts the user to input the goal number of pounds of chocolate to sell.
         System.out.println("Enter the goal number of pounds to sell:");
+        //This defines the goal as their input.
         int goalPounds = sc.nextInt();
+        //This sets the counting mechanism of pounds sold to 0.
         int counter = 0;
+        //This sets the number of 5 pound bars sold to 0.
         int numberFivePoundsSold = 0;
+        //This sets the number of 1 pound bars sold to 0.
         int numberOnePoundsSold = 0;
-
+        //This starts the if statement to execute if there are remaining 5 pound bars.
         if (fivePound > 0) {
+            //This executes the following if another 5 pound bar can be added
+            // without selling too much or running out of 5 pound bars.
             while (counter + 5 <= goalPounds && numberFivePoundsSold < fivePound) {
+                //This adds 5 to the pounds of chocolate sold.
                 counter = counter + 5;
+                //This adds 1 5 pound bar to the number of 5 pound bars sold.
                 numberFivePoundsSold++;
             }
         }
-
+        //This starts the if statement to execute if there are remaining 1 pound bars.
         if (onePound > 0) {
+            //This executes the following if another 1 pound bar can be added
+            // without selling too much or running out of 1 pound bars.
             while (counter + 1 <= goalPounds && numberOnePoundsSold < onePound) {
+                //This adds 1 to the pounds of chocolate sold.
                 counter++;
+                //This adds a single 1 pound bar to the number of 1 pound bars sold.
                 numberOnePoundsSold++;
             }
         }
-
+        //This starts the if statement to execute if the counter eventually hits the goal pounds sold.
         if (counter == goalPounds) {
+            //This defines the integer i as the total number of bars sold.
             int i = numberFivePoundsSold + numberOnePoundsSold;
+            //This returns the value of i.
             return i;
+            //This executes the backup plan if the counter does not ever hit the goal chocolate sold.
         } else {
+            //This returns -1 to inform the user that it is not possible to sell the goal with the inventory.
             return -1;
         }
     }
